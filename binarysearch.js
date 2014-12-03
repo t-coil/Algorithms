18f35e6f815005fc3afcb119dd9ca719146702a0
t-coil <t.e.siegel@gmail.com>
2014-12-03 14:25:46 -0800

    error handling and comments added

--- binarysearch.js
+++ binarysearch.js
@@ -1,12 +1,16 @@
-// When this function is called, num must always be set to one for the correct result.
-
 var binarySearch = function(arr, val, num) {
+    num = num || 0;
+
+    if (arr[0] === val) {
+        return 0;
+    }
+
     middle = Math.floor(arr.length / 2);
 
     if (arr[middle] === val) {
-        return middle+num;
+        return middle + num;
     } else if (arr.length === 1) {
-        return "Value not found"
+        return -1;
     } else if (arr[middle] > val) {
         binarySearch(arr.slice(0, middle), val, num);
     } else {
@@ -14,3 +18,11 @@ var binarySearch = function(arr, val, num) {
         binarySearch(arr.slice(middle, arr.length), val, num);
     }
 }
+
+// Examples:
+
+// var exampleArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// binarySearch(exampleArray, 4) => 3
+// binarySearch(exampleArray, 9) => 8
+// binarySearch(exampleArray, 11) => -1
