75ff0fbd1610e2e07771c5835f05d77437623a46
t-coil <t.e.siegel@gmail.com>
2014-12-03 14:12:08 -0800

    recursive binary search

---
+++ binarysearch.js
@@ -0,0 +1,14 @@
+var binarySearch = function(arr, val, num) {
+    console.log(arr);
+    middle = Math.floor(arr.length / 2);
+
+    if (arr[middle] === val) {
+        console.log(middle+num)
+        return middle;
+    } else if (arr[middle] > val) {
+        binarySearch(arr.slice(0, middle), val, num);
+    } else {
+        num += arr.slice(0, middle).length;
+        binarySearch(arr.slice(middle, arr.length), val, num);
+    }
+}
