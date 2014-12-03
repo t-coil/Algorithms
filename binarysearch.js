5a4cebf04ec16adb44924835255895a75c43af91
t-coil <t.e.siegel@gmail.com>
2014-12-03 14:15:25 -0800

    logs removed

--- binarysearch.js
+++ binarysearch.js
@@ -1,12 +1,12 @@
 // When this function is called, num must always be set to one for the correct result.
 
 var binarySearch = function(arr, val, num) {
-    console.log(arr);
     middle = Math.floor(arr.length / 2);
 
     if (arr[middle] === val) {
-        console.log(middle+num)
-        return middle;
+        return middle+num;
+    } else if (arr.length === 1) {
+        return "Value not found"
     } else if (arr[middle] > val) {
         binarySearch(arr.slice(0, middle), val, num);
     } else {
